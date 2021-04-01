a71b50a834e751b4dac36ff009dfa136548d9eee
Amanda <mandygudlwa@gmail.com>
2021-04-01 10:52:54 +0200

    updates

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -1,6 +1,11 @@
 describe('countAllFromTown' , function(){
     it('return number registration numbers from Cape Town(CA).' , function(){
-        assert.equal(countAllFromTown('CA182736 CY523519 CJ81232854678GP 79974L EC5768 23662MP'), 0);
+        assert.equal(countAllFromTown('CA182736 CY523519 CJ81232854678GP 79974L EC5768 23662MP','CA'), 1);
+
+    });
+
+    it('return number registration numbers from Gauteng(GP).' , function(){
+        assert.equal(countAllFromTown('CA182736 CY523519 C81232854678 79974L EC5768 23662MP','CY'), 0);
 
     });
 
